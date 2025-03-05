7a7dac0f388c7c4d38f8f0cf52f1b1a9cd2de0a3
tracyn09 <tracyn0099@gmail.com>
2025-03-05 10:56:22 -0500

    Task 3–Bulk Update on Employee Cards

--- cc_13.js
+++ cc_13.js
@@ -17,9 +17,10 @@ employeePosition.textContent = position
 //Remove button
 const removeButton = document.createElement(`button`)
 removeButton.textContent = `Remove`
-removeButton.onclick = function() {
+removeButton.onclick = function () {
     card.remove()
 }
+
 //Appending employee card to employee container
 card.appendChild(employeeName)
 card.appendChild(employeePosition)
@@ -27,9 +28,25 @@ card.appendChild(removeButton)
 container.appendChild(card)
 }
 
+//Task 3
+//Updating employee cards
+function updateEmployeeCards() {
+const employeeCards = document.querySelectorAll(`.employee-card`)
+
+//Convert NodeList to array
+const employeeCardsArray = Array.from(employeeCards)
+employeeCardsArray.forEach(card=> {
+    card.style.border = `1px solid black`
+    card.style.backgroundColor = `pink`
+    card.style.margin = `10px`
+})
+}
+
 //Test Case for Task 2
 window.onload = function() {
     addEmployeeCard('Marvin Guerrero', 'Software Engineer')
     addEmployeeCard('Teresa Nguyen', 'Account Executive')
     addEmployeeCard(`Joseph Tornincaso`, `Chief Financial Officer`)
+//Test Case for Task 3
+updateEmployeeCards()
 }
